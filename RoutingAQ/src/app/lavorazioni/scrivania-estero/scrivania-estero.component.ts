1393292e5f6692cfbb23602bfa12d028210159ee
Sparsh Singh <sparsh262002@gmail.com>
2026-01-07 18:21:38 +0530

    sibling communication

--- RoutingAQ/src/app/lavorazioni/scrivania-estero/scrivania-estero.component.ts
+++ RoutingAQ/src/app/lavorazioni/scrivania-estero/scrivania-estero.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataSharingService } from '../../services/data-sharing.service';
 
 @Component({
   selector: 'app-scrivania-estero',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./scrivania-estero.component.css']
 })
 export class ScrivaniaEsteroComponent {
+constructor(private dataSharingService: DataSharingService) {}
+receivedRow: any;
 
+  ngOnInit() {
+    this.dataSharingService.selectedRow$.subscribe(row => {
+      console.log('ESTERO: Received row', row);
+      this.receivedRow = row;
+    });
+  }
 }
--- RoutingAQ/src/app/lavorazioni/scrivania-italia/scrivania-italia.component.ts
+++ RoutingAQ/src/app/lavorazioni/scrivania-italia/scrivania-italia.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataSharingService } from '../../services/data-sharing.service';
 
 @Component({
   selector: 'app-scrivania-italia',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./scrivania-italia.component.css']
 })
 export class ScrivaniaItaliaComponent {
+  constructor(private dataSharingService: DataSharingService) {}
+
 columns = ['id', 'client', 'amount'];
 data = [
     { id: 1, client: 'Mario Rossi', amount: 1000 },
@@ -16,8 +19,14 @@ data = [
   selectedRow: any;
 
 onRowSelected(row: any) {
-  console.log('PARENT: Row received', row);
+  // console.log('PARENT: Row received', row);
+  // this.selectedRow = row;
+
+  console.log('ITALIA (Parent): Row selected or received', row);
   this.selectedRow = row;
+
+  this.dataSharingService.shareSelectedRow(row);
+
 }
 
 }
