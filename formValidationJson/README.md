f547c3ad8933a69c868e20ffc9752a7605313519
Sparsh Singh <sparsh262002@gmail.com>
2026-01-07 10:46:17 +0530

    Assignment 1 - Version 3 (Final)

--- formValidationJson/README.md
+++ formValidationJson/README.md
@@ -1,4 +1,4 @@
-# FormValidation
+# FormValidationJson
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.11.
 
--- formValidationJson/src/app/app.component.spec.ts
+++ formValidationJson/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'formValidation'`, () => {
+  it(`should have as title 'formValidationJson'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('formValidation');
+    expect(app.title).toEqual('formValidationJson');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('formValidation app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('formValidationJson app is running!');
   });
 });
