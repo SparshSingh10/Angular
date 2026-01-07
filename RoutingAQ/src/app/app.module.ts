8f802bd25db18f1ffdd39e6cb5612f34f85850c3
Sparsh Singh <sparsh262002@gmail.com>
2026-01-07 15:42:19 +0530

    reusable-table-skeleton

--- RoutingAQ/src/app/app.module.ts
+++ RoutingAQ/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './shared/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
